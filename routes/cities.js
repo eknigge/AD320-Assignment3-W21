d6250b45d65ffceee8b99747afedb661803d3361
swang93 <swang93@uw.edu>
2021-01-30 19:35:08 -0800

    added routes to cities.js

--- routes/cities.js
+++ routes/cities.js
@@ -1,4 +1,30 @@
+"use strict";
+
 const express = require('express');
 const router = express.Router();
+const cities = require('../data/cities.json');
+
+/**
+ * Retrieves all cities
+ */
+router.get('/', function(req, res){
+    res.json(cities);
+})
+
+/** 
+ * Retrieves a specific city by ID, and return the corresponding JSON 
+ */
+router.get('/:id', function(req, res){
+    let reqID = parseInt(req.params.id);
+    let result = {};
+    
+    for(let index in cities){
+        let cityID = parseInt(cities[index]["id"]);
+        if(cityID === reqID){
+            result = cities[index];
+        }
+    }
+    res.json(result);
+})
 
 module.exports = router;
